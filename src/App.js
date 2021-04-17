d96ec3ab4bf05016bfb1914c54a5d4d341f9fe83
Misha Batsiashvili <mishabatsiashvili@yahoo.com>
2021-04-17 11:26:13 +0400

    added history, float numbers and sign changer. also improved ui

--- src/App.js
+++ src/App.js
@@ -11,11 +11,17 @@ const App = () => {
 	4. select next operation -> calculate previous operation / press "=" and calculate current operation
 	*/
 
-	const [firstNum, setFirstNum] = useState(0);
-	const [secondNum, setSecondNum] = useState(0);
+	const [firstNum, setFirstNum] = useState('0');
+	const [secondNum, setSecondNum] = useState('0');
 
 	// this should store operation index
 	const [operationIndex, setOperationIndex] = useState(null);
+	// this store operation index to highlight it on the calculator UI
+	const [uiOperationIndex, setUiOperationIndex] = useState(null);
+	// /.
+
+	//calculations' history array
+	const [history, setHistory] = useState([]);
 	// /.
 
 	/*
@@ -54,17 +60,42 @@ const App = () => {
 
 	const numberClickHandler = (num) => {
 		if (step === 0) {
-			if (firstNum === 0) {
+			if (firstNum === '0') {
 				setFirstNum(num.toString());
 			} else {
 				setFirstNum(firstNum + num.toString());
 			}
 		} else if (step === 1 || step === 2) {
 			setStep(2);
-			if (secondNum === 0) {
+			setUiOperationIndex(null);
+			if (secondNum === '0') {
 				setSecondNum(num.toString());
 			} else {
 				setSecondNum(secondNum + num.toString());
+			}
+		}
+	}
+
+	const makeFloatHandler = () => {
+		if (step === 0) {
+			setFirstNum(firstNum + '.');
+		} else if (step === 2) {
+			setSecondNum(secondNum + '.');
+		}
+	}
+
+	const makeNegativeHandler = () => {
+		if (step === 0) {
+			if(firstNum.includes('-')){
+				setFirstNum(firstNum.substring(1, firstNum.length));
+			} else {
+				setFirstNum('-' + firstNum);
+			}
+		} else if (step === 2) {
+			if(secondNum.includes('-')){
+				setSecondNum(secondNum.substring(1, secondNum.length));
+			} else {
+				setSecondNum('-' + secondNum);
 			}
 		}
 	}
@@ -73,47 +104,61 @@ const App = () => {
 		if (step === 0) {
 			setStep(1);
 			setOperationIndex(opIndex);
+			setUiOperationIndex(opIndex);
 		} else if (step === 1) {
 			if (opIndex !== operationIndex) {
 				setOperationIndex(opIndex);
+				setUiOperationIndex(opIndex);
 			}
 		} else if (step === 2) {
 			calculateClickHandler(opIndex);
 		}
 	}
 
+	const setHistStr = (op, res) => {
+		setHistory([`${firstNum} ${op} ${secondNum} = ${res}`, ...history])
+	}
+
 	const calculateClickHandler = (nextOperation = null) => {
 
-		const first = parseInt(firstNum);
-		const second = secondNum ? parseInt(secondNum) : first;
+		const first = parseFloat(firstNum);
+		const second = secondNum ? parseFloat(secondNum) : first;
 		let result = 0;
 
 		switch (operationIndex) {
 			case 0:
 				result = first / second;
+				setHistStr('/', result);
 				break;
 			case 1:
 				result = first * second;
+				setHistStr('*', result);
 				break;
 			case 2:
 				result = first + second;
+				setHistStr('+', result);
 				break;
 			case 3:
 				result = first - second;
+				setHistStr('-', result);
 				break;
+			default:
+				result = first;
+			
 		}
 
 
-		setFirstNum(result);
-		setSecondNum(0);
+		setFirstNum(result.toString());
+		setSecondNum('0');
 		setOperationIndex(nextOperation);
+		setUiOperationIndex(nextOperation);
 		setStep(nextOperation ? 1 : 0);
 	}
 
 
 	const clearAll = () => {
-		setFirstNum(0);
-		setSecondNum(0);
+		setFirstNum('0');
+		setSecondNum('0');
 		setOperationIndex(null);
 		setStep(0);
 	}
@@ -136,7 +181,12 @@ const App = () => {
 		<div className="App">
 			<h3 className="main-input" dangerouslySetInnerHTML={{ __html: renderMainInput() }} />
 			<div className="numbersWrp">
-				<div className="number" onClick={clearAll}>AC</div>
+				<div className="functions-wrp">
+					<div className="number" onClick={clearAll}>AC</div>
+					<div className="number" onClick={makeFloatHandler}>.</div>
+					<div className="number" onClick={makeNegativeHandler}>+/-</div>
+				</div>
+
 				<Numbers
 					numbers={numArr.reverse()}
 					numColumns={numColumns}
@@ -147,7 +197,7 @@ const App = () => {
 					{operations.map((el, i) => (
 						<div
 							key={i}
-							className={`operation ${i === operationIndex ? 'operation-selected' : ''}`}
+							className={`operation ${i === uiOperationIndex ? 'operation-selected' : ''}`}
 							dangerouslySetInnerHTML={{ __html: el.value }}
 							onClick={() => operationClickHandler(i)}
 						/>
@@ -156,6 +206,17 @@ const App = () => {
 				</div>
 
 			</div>
+
+
+			<div className="history">
+				<h3>History</h3>
+				{
+					history.length > 0
+					? history.map(el => <div className="history-item">{el}</div>)
+					: <div className="history-item">No History</div>
+				}
+			</div>
+
 		</div>
 	);
 }
